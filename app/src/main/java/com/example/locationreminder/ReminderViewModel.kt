6bf248c2a7a48b6027e31e6a53d6416b488918cb
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-05-26 21:19:09 +0100

    Finshed Reminder ViewModel

--- app/src/main/java/com/example/locationreminder/ReminderViewModel.kt
+++ app/src/main/java/com/example/locationreminder/ReminderViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.locationreminder
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.locationreminder.dto.ReminderDTO
+import com.example.locationreminder.utils.Result
 import kotlinx.coroutines.launch
 
 class ReminderViewModel(private val dataSource: ReminderDataSource) : ViewModel() {
@@ -12,6 +15,28 @@ class ReminderViewModel(private val dataSource: ReminderDataSource) : ViewModel(
     fun loadReminders() {
         viewModelScope.launch {
             val result = dataSource.getReminders()
+            when (result) {
+                is Result.Success<*> -> {
+                    val dataList = ArrayList<ReminderData>()
+                    dataList.addAll((result.data as List<ReminderDTO>).map {
+                        ReminderData(
+                            it.title,
+                            it.description,
+                            it.location,
+                            it.latitude,
+                            it.longitude,
+                            it.id
+                        )
+                    })
+                    reminderList.value = dataList
+                }
+                is Result.Error -> Log.d("ReminderViewModel", "${result.message}")
+            }
+            invalidateShowNoData()
         }
+    }
+
+    private fun invalidateShowNoData() {
+        reminderList.value == null || reminderList.value!!.isEmpty()
     }
 }
